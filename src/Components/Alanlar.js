f4a671c2b503c042359005bf207ef4130baafb87
chnkmlogt <kmlchnogt>
2020-03-27 00:52:07 +0300

    cc

--- src/Components/Alanlar.js
+++ src/Components/Alanlar.js
@@ -2,21 +2,23 @@ import React from "react";
 import Yuzde from "./Yuzde";
 import Adet from "./Adet";
 import Litre from "./Litre";
+import Alan from "./Alan";
 
 const Alanlar = {
   lt: Litre,
   adet: Yuzde,
   yuzde: Adet
 };
-export default alan => {
-  if (typeof Alanlar[alan.alan.birim] !== "undefined") {
-    return React.createElement(Alanlar[alan.alan.birim], {
-      alanAd: alan.alan.alanAd,
-      miktar: alan.alan.miktar
+export default alanlar => {
+  if (typeof Alanlar[alanlar.alan.birim] !== "undefined") {
+    return React.createElement(Alanlar[alanlar.alan.birim], {
+      alanAd: alanlar.alan.alanAd,
+      miktar: alanlar.alan.miktar
     });
   }
-  return React.createElement(Adet, {
-    alanAd: alan.alan.alanAd,
-    miktar: alan.alan.miktar
+  return React.createElement(Alan, {
+    alanAd: alanlar.alan.alanAd,
+    miktar: alanlar.alan.miktar,
+    birim: alanlar.alan.birim
   });
 };
--- src/Components/Arac.js
+++ src/Components/Arac.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Alanlar from "./Alanlar";
 
 const Arac = props => {
